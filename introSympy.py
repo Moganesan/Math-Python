d7c1018b5375e1335cd5b6ec3933f04f3bccdd34
moganesan <developer.moganesan@gmail.com>
2022-01-26 11:47:42 +0530

    commit

---
+++ introSympy.py
@@ -0,0 +1,54 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "80654ef1",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/latex": [
+       "$\\displaystyle X^{Y}$"
+      ],
+      "text/plain": [
+       "X**Y"
+      ]
+     },
+     "metadata": {},
+     "output_type": "display_data"
+    }
+   ],
+   "source": [
+    "import sympy as sym\n",
+    "from IPython.display import display,Math\n",
+    "\n",
+    "x = sym.symbols(\"X\")\n",
+    "y = sym.symbols(\"Y\")\n",
+    "\n",
+    "display(x**y)"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
